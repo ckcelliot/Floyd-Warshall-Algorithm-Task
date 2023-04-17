7401355bc23c184937df9f7ca73f0f2b2fe4025d
ckcelliot <130892458+ckcelliot@users.noreply.github.com>
2023-04-17 05:36:11 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,33 +1,51 @@
-import timeit
+import unittest
 import sys
-import itertools
 
-# Import floyd_rec.py and floyd.py as modules
-import floyd_rec
-import floyd
+# Import the functions to be tested
+from floyd_rec import floyd_recursive
+from floyd import floyd
 
-# Define the graph data for testing
-NO_PATH = sys.maxsize
-graph = [
-    [0, 7, NO_PATH, 8],
-    [NO_PATH, 0, 5, NO_PATH],
-    [NO_PATH, NO_PATH, 0, 2],
-    [NO_PATH, NO_PATH, NO_PATH, 0]
-]
-MAX_LENGTH = len(graph[0])
+class TestFloydAlgorithm(unittest.TestCase):
 
-# Performance test for file1.py
-def floyd_rec_perf():
-    floyd_rec.floyd(graph)
+    def setUp(self):
+        # Initialize test data
+        self.NO_PATH = sys.maxsize
+        self.graph = [
+            [0, 7, self.NO_PATH, 8],
+            [self.NO_PATH, 0, 5, self.NO_PATH],
+            [self.NO_PATH, self.NO_PATH, 0, 2],
+            [self.NO_PATH, self.NO_PATH, self.NO_PATH, 0]
+        ]
+        self.MAX_LENGTH = len(self.graph[0])
 
-elapsed_time_file1 = timeit.timeit(floyd_rec_perf, number=1)
+    def test_floyd_rec(self):
+        # Test case 1: Start and end nodes are the same
+        distance = [[self.NO_PATH] * self.MAX_LENGTH for _ in range(self.MAX_LENGTH)]
+        intermediate = 0
+        start_node = 0
+        end_node = 0
+        floyd_recursive(distance, intermediate, start_node, end_node)
+        self.assertEqual(distance[start_node][end_node], 0)
 
-# Performance test for file2.py
-def floyd_perf():
-    floyd.floyd(graph)
+        # Test case 2: Start node is different from end node
+        distance = [[self.NO_PATH] * self.MAX_LENGTH for _ in range(self.MAX_LENGTH)]
+        intermediate = 1
+        start_node = 0
+        end_node = 1
+        floyd_recursive(distance, intermediate, start_node, end_node)
+        self.assertEqual(distance[start_node][end_node], 7)
 
-elapsed_time_file2 = timeit.timeit(floyd_perf, number=1)
+    def test_floyd(self):
+        # Test case 1: Check if floyd function updates distance matrix correctly
+        distance = [row[:] for row in self.graph]
+        floyd(distance)
+        expected_distance = [
+            [0, 7, 5, 8],
+            [self.NO_PATH, 0, 5, 7],
+            [self.NO_PATH, self.NO_PATH, 0, 2],
+            [self.NO_PATH, self.NO_PATH, self.NO_PATH, 0]
+        ]
+        self.assertEqual(distance, expected_distance)
 
-# Print the results
-print("Elapsed time for Floyd Algorithm using Recursion: {} seconds".format(elapsed_time_file1))
-print("Elapsed time for Floyd Algorithm using Iteration: {} seconds".format(elapsed_time_file2))
+if __name__ == '__main__':
+    unittest.main()
